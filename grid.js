46884c902b7170d12c3232294a5a1e00f4856e33
brito <fbrito@ext-intertrust.com>
2013-01-10 19:48:55 -0800

    function recognize

---
+++ grid.js
@@ -0,0 +1,18 @@
+/*! //github.com/brito/grid.js/blob/master/README.md */
+function grid () {
+
+    /** mix of assert, error and info:
+    something (truthy) & joined arguments */
+    function recognize ( 
+            something // a test (truthy)
+            // arguments joined and appended
+    ){ if (debug)
+       // output
+       console
+       // TODO add delta and warn, debug, dir
+       [ ['error','info'] [+!!something] ] (
+                // a brute join
+               [].join.call(arguments,' ')); }
+
+
+var debug = !0}
